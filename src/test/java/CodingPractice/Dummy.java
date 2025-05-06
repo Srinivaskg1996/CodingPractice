7613a5b5ac0341d19752cd86b97e44fcc21d729d
Srinivas K G <Srinivaskg2018@gmail.com>
2025-05-06 13:22:06 +0530

    Testing3

---
+++ src/test/java/CodingPractice/Dummy.java
@@ -0,0 +1,29 @@
+package CodingPractice;
+
+import java.util.Arrays;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		
+			
+			int a[]= {5,3,2,4,1};
+			
+			for(int i=0; i<a.length-1; i++)
+			{
+				for(int j=0; j<a.length-1;j++)
+				{
+					if(a[j]<a[j+1])
+					{
+						int temp= a[j];
+						a[j]=a[j+1];
+						a[j+1]=temp;
+					}
+				}
+			}
+			System.out.println(Arrays.toString(a));
+		}
+
+	}
+
+}
